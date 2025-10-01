cf84261b5007d25c67e97383cb7a222aecbe6416
VishorGit07 <vishorgunasekaran@gmail.com>
2025-10-01 12:35:18 +0530

    Update App.jsx and neww.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,7 @@ import ScrollToTop from "./components/ScrollToTop";
 import "./style.css";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Neww from "./components/samplly/neww";
 
 function App() {
   const [load, upadateLoad] = useState(true);
@@ -39,7 +40,9 @@ function App() {
           <Route path="/project" element={<Projects />} />
           <Route path="/about" element={<About />} />
           <Route path="/resume" element={<Resume />} />
+          <Route path="/new" element={<Neww />} />
           {/* <Route path="*" element={<Navigate to="/"/>} /> */}
+          
         </Routes>
        
       </div>
